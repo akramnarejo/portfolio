eab5875bdf7433c5b5f68218c9a012461d2a757b
Muhammad Akram <makramnarejo@gmail.com>
2024-04-29 18:33:37 +0500

    Fix: app title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import { Analytics } from '@vercel/analytics/react';
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Portfolio: Akram Narejo",
+  title: "Muhammad Akram",
   description: "",
 };
 
