b74f51d08b6436ba11aad9335aa5aedc1ff0c449
Muhammad Akram <ak.narejo@gmail.com>
2024-04-29 20:04:04 +0400

    fix: spaces after title"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
           G&apos;day, I&apos;m Akram <span className="animate-pulse duration-1000">👋</span>
         </h1>
         <p className="w-full [font-size:_clamp(2.5rem,4vw,6rem)] mt-1  text-[#666]">
-          A{" "}<span className="dark:bg-[#111] bg-[#eee] rounded-lg border border-[#333] text-blue-800">Frontend Engineer 👨‍💻 </span>{" "}crafting visuals and user experience,
+          A{" "}<span className="dark:bg-[#111] bg-[#eee] rounded-lg border border-[#333] text-blue-800">Frontend Engineer 👨‍💻</span>{" "}crafting visuals and user experience,
           Web3 enthusiast and <span className="text-blue-800">#Photographer</span> 📸. Based in Dubai 🇦🇪.
         </p>
         <p className="w-full mt-2 tracking-wide text-[#666] italic [font-size:_clamp(1.2rem,1.5vw,2rem)]">
@@ -26,13 +26,13 @@ export default function Home() {
         </p>
       </section>
       <div className="flex gap-4 box-border mt-4">
-        <Link href='https://linkedin.com/in/akramnarejo' target="_blank" className="text-2xl md:text-3xl hover:text-blue-500">
+        <Link href='https://linkedin.com/in/akramnarejo' target="_blank" className="text-2xl md:text-3xl hover:text-blue-500 duration-1000">
           <FaLinkedin />
         </Link>
-        <Link href='https://github.com/akramnarejo' target="_blank" className="text-2xl md:text-3xl hover:text-gray-500">
+        <Link href='https://github.com/akramnarejo' target="_blank" className="text-2xl md:text-3xl hover:text-gray-500 duration-1000">
           <FaGithub />
         </Link>
-        <Link href='https://instagram.com/akramnarejo' target="_blank" className="text-2xl md:text-3xl hover:text-pink-600">
+        <Link href='https://instagram.com/akramnarejo' target="_blank" className="text-2xl md:text-3xl hover:text-pink-600 duration-1000">
           <FaInstagram />
         </Link>
       </div>
